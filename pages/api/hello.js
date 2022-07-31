6d12b6ee772c6b68b9f0b6b7b601e57da0e6d38b
Ikbal Yaduar Taupik <Ikbal.ytaufik@gmail.com>
2022-07-31 12:59:31 +0700

    add test GET api from mongo db

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,44 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import clientPromise from "../../lib/mongodb"
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function (req, res){
+
+  // `await clientPromise` will use the default database passed in the MONGODB_URI
+  // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
+  //
+  // `const client = await clientPromise`
+  // `const db = client.db("myDatabase")`
+  //
+  // Then you can execute queries against your database like so:
+  // db.find({}) or any of the MongoDB Node Driver commands
+
+
+  const client = await clientPromise
+  const db_e_learning_db = await client.db("e_learning_db")
+  const userTbl = await db_e_learning_db.collection("userTbl")
+  const data = userTbl.find({})
+    
+  const result = await data.toArray()
+  if (req.method === "GET"){
+    res.json(result)
+  } else if (req.method === "POST") {
+    const name = req.body.name
+    const password = req.body.password
+    const email = req.body.email
+    const nip = req.body.nip
+    const userLast_update = new Date()
+
+    await createListing(client, {
+      name: name,
+      password: password,
+      email: email,
+      nip: nip,
+      last_update: userLast_update
+    })
+
+    async function createListing(client, newListing) {
+      const result = client.db("e_learning_db").collection("userTbl").insertOne(newListing)
+      console.log(`New Listing created with the following id : ${result.insertedId}`)
+    }
+
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import clientPromise from '../lib/mongodb'
 
-export default function Home() {
+export default function Home({isConnected}) {
   return (
     <div className={styles.container}>
       <Head>
@@ -12,7 +13,18 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+
         <h1>Hello</h1>
+
+        {isConnected ? (
+          <h2 className="subtitle">You are connected to MongoDB</h2>
+        ) : (
+          <h2 className="subtitle">
+            You are NOT connected to MongoDB. Check the <code>README.md</code>{' '}
+            for instructions.
+          </h2>
+        )}
+        
       </main>
 
       <footer className={styles.footer}>
@@ -29,4 +41,27 @@ export default function Home() {
       </footer>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  try {
+    await clientPromise
+    // `await clientPromise` will use the default database passed in the MONGODB_URI
+    // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
+    //
+    // `const client = await clientPromise`
+    // `const db = client.db("myDatabase")`
+    //
+    // Then you can execute queries against your database like so:
+    // db.find({}) or any of the MongoDB Node Driver commands
+
+    return {
+      props: { isConnected: true },
+    }
+  } catch (e) {
+    console.error(e)
+    return {
+      props: { isConnected: false },
+    }
+  }
 }
