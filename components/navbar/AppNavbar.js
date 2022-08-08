089b331e0577d54258712b438418c045ba523576
Ikbal Yaduar Taupik <Ikbal.ytaufik@gmail.com>
2022-08-08 09:02:23 +0700

    change

--- components/navbar/AppNavbar.js
+++ components/navbar/AppNavbar.js
@@ -21,45 +21,30 @@ export default function AppNavbar() {
   const show = state.menu ? " show" : ""
 
   return (
-    // <div className="container">
-    //   {/* <h3>nav</h3> */}
-    //   <div className="row">
-    //     <div className="col-auto">
-    //       <Link href="/" passHref>
-    //         <a style={{textDecoration: 'none'}}>Home</a>
-    //       </Link>
-    //     </div>
-    //     <div className="col-auto">
-    //       <Link href="/about" passHref>
-    //         <a style={{textDecoration: 'none'}}>About</a>
-    //       </Link>
-    //     </div>
-    //   </div>
-    // </div>
     <nav className="navbar navbar-expand-lg bg-light">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
-    <button className="navbar-toggler" onClick={toggleMenu} type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className={"collapse navbar-collapse" + show} id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <Link href={'/'} passHref>
-          <a className="nav-link" aria-current="page">Home</a>
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link href={'/about'} passHref>
-          <a className="nav-link">About</a>
-          </Link>
-        </li>
-      </ul>
-      <form className="d-flex" role="search">
-        {!session ? <button onClick={() => signIn()} className="btn btn-outline-success" type="submit">Sign in</button> : <h3><button className="btn btn-outline-danger" onClick={() => signOut()}>Sign Out</button></h3>}
-      </form>
-    </div>
-  </div>
-</nav>
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">Navbar</a>
+        <button className="navbar-toggler" onClick={toggleMenu} type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className={"collapse navbar-collapse" + show} id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link href={'/'} passHref>
+              <a className="nav-link" aria-current="page">Home</a>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link href={'/about'} passHref>
+              <a className="nav-link">About</a>
+              </Link>
+            </li>
+          </ul>
+          <form className="d-flex" role="search">
+            {!session ? <button onClick={() => signIn()} className="btn btn-outline-success" type="submit">Sign in</button> : <h3><button className="btn btn-outline-danger" onClick={() => signOut()}>Sign Out</button></h3>}
+          </form>
+        </div>
+      </div>
+    </nav>
   )
 }
