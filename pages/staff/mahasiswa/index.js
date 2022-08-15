816c63d9ce7ec555d9018b6ea6267eba83d8e67c
Ikbal Yaduar Taupik <Ikbal.ytaufik@gmail.com>
2022-08-16 06:40:41 +0700

    move modal

--- pages/staff/mahasiswa/index.js
+++ pages/staff/mahasiswa/index.js
@@ -15,6 +15,18 @@ export default function MahasiswaPage(){
 
     const [mahasiswa, setData] = useState(null)
     const [isLoading, setLoading] = useState(false)
+    const [state, setState] = useState({
+        modal: false,
+        display: false,
+        isOpen: false,
+    })
+
+    const toggleModal = () => {
+        setState({
+          ...state,
+          modal: !state.modal
+        })
+    }
 
     async function fetchMhs(){
         const response = await fetch('/api/mahasiswa')
@@ -77,6 +89,9 @@ export default function MahasiswaPage(){
         }
     }, [])
 
+    const show = state.modal ? " show" : ""
+    const display = state.modal ? "block" : "none"
+
     if(isLoading) return <p>Loading...</p>
     if(!mahasiswa) return <p>No profile data</p>
 
@@ -91,7 +106,87 @@ export default function MahasiswaPage(){
         </div>
         <div style={{'padding': '20px'}}>
             <div style={{'padding': '20px'}} className="row">
-                <Modal/>
+                <button onClick={toggleModal} style={{'width': 'auto'}} type="button" className="btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    input
+                </button>
+                    
+                <div className={`modal fade ${show}`} style={{'display': `${display}`}} id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div className="modal-dialog modal-xl">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                        <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                        <button type="button" className="btn-close" data-bs-dismiss="modal" onClick={toggleModal} aria-label="Close"></button>
+                        </div>
+                        
+                        <div className="modal-body">
+                        <form className="row g-3">
+                            <div className="col-md-4">
+                            <label htmlFor="inputEmail4" className="form-label">Name</label>
+                            <input type="email" className="form-control"/>
+                            </div>
+
+                            <div className="col-md-4">
+                            <label htmlFor="inputPassword4" className="form-label">NIM</label>
+                            <input type="password" className="form-control" />
+                            </div>
+                                
+                            <div className="col-md-4">
+                            <label htmlFor="inputEmail4" className="form-label">Kelas</label>
+                            <input type="email" className="form-control"/>
+                            </div>
+                        
+                            <div className="col-md-6">
+                            <label htmlFor="inputEmail4" className="form-label">Email</label>
+                            <input type="email" className="form-control"/>
+                            </div>
+                        
+                            <div className="col-md-6">
+                            <label htmlFor="inputPassword4" className="form-label">Password</label>
+                            <input type="password" className="form-control" />
+                            </div>
+                        
+                            <div className="col-12">
+                            <label htmlFor="inputAddress" className="form-label">Address</label>
+                            <input type="text" className="form-control" id="inputAddress" placeholder="Jl. Abc, Kec. Example"/>
+                            </div>
+                                
+                            <div className="col-md-6">
+                            <label htmlFor="inputCity" className="form-label">Kab/Kota</label>
+                            <input type="text" className="form-control" id="inputCity"/>
+                            </div>
+                        
+                            <div className="col-md-4">
+                            <label htmlFor="inputState" className="form-label">Prov</label>
+                            <select id="inputState" defaultValue="1" className="form-select">
+                                <option value="1">Choose...</option>
+                                <option>...</option>
+                            </select>
+                            </div>
+                                
+                            <div className="col-md-2">
+                            <label htmlFor="inputZip" className="form-label">Zip</label>
+                            <input type="text" className="form-control" id="inputZip"/>
+                            </div>
+                                
+                            <div className="col-12">
+                            <div className="input-group mb-3">
+                                <input type="file" className="form-control" id="inputGroupFile02"/>
+                                <label className="input-group-text" htmlFor="inputGroupFile02">Upload</label>
+                            </div>
+                            </div>
+                            
+                            <div className="col-12">
+                            <div className="modal-footer">
+                                <button onClick={toggleModal} type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="submit" className="btn btn-primary">Save changes</button>
+                            </div>
+                            </div>
+                        </form>
+
+                        </div>
+                    </div>
+                    </div>
+                </div>
             </div>
 
             <hr/>
