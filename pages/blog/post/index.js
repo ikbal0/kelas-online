0b0729de6ec34d60cd9789a1403c12fa024dc510
Ikbal Yaduar Taupik <Ikbal.ytaufik@gmail.com>
2022-10-02 07:31:14 +0700

    update

--- pages/blog/post/index.js
+++ pages/blog/post/index.js
@@ -1,5 +1,7 @@
+import axios from "axios";
 import { getSession } from "next-auth/react";
-import { useState } from "react";
+import Head from "next/head";
+import { useEffect, useState } from "react";
 
 function PostPage(){
     const Nav = () => {
@@ -8,7 +10,32 @@ function PostPage(){
             display: false,
             isOpen: false,
         })
+
+        const [selectedFile, setSelectedFile] = useState()
+        const [preview, setPreview] = useState()
         
+        useEffect(() => {
+            if(!selectedFile){
+                setPreview(undefined)
+                return
+            }
+    
+            const objectUrl = URL.createObjectURL(selectedFile)
+            setPreview(objectUrl)
+    
+            return () => URL.revokeObjectURL(objectUrl)
+        }, [selectedFile])
+    
+        const onSelectFile = e => {
+            if (!e.target.files || e.target.files.length === 0) {
+                setSelectedFile(undefined)
+                return
+            }
+    
+            // I've kept this example simple by using the first image instead of multiple
+            setSelectedFile(e.target.files[0])
+        }
+
         const toggleModal = () => {
             setState({
             ...state,
@@ -34,6 +61,7 @@ function PostPage(){
         
             formData.append("n_speakers", event.target.n_speakers.value)
             formData.append("uploaded_file", event.target.uploaded_file.files[0])
+            formData.append("file_name", event.target.uploaded_file.files[0].name)
         
             // console.log(event.target.uploaded_file.files[0])
         
@@ -59,7 +87,7 @@ function PostPage(){
             // Send the form data to our forms API on Vercel and get a response.
             // const response = await fetch(endpoint, formData, options)
             // const response = await fetch(endpoint, options)
-            // const response = await axios.post(endpoint, formData, options)
+            const response = await axios.post(endpoint, formData, options)
         
             // Get the response data from server as JSON.
             // If server returns the name submitted, that means the form works.
@@ -74,41 +102,55 @@ function PostPage(){
           
         return (
             <>
-              <button onClick={toggleModal} style={{'width': 'auto'}} type="button" className="btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                input
-              </button>
-                
-              <div className={`modal fade ${show}`} style={{'display': `${display}`}} id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div className="modal-dialog modal-xl">
-                  <div className="modal-content">
-                    <div className="modal-header">
-                      <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
-                      <button type="button" className="btn-close" data-bs-dismiss="modal" onClick={toggleModal} aria-label="Close"></button>
-                    </div>
-                      
-                    <div className="modal-body">
-                    <form onSubmit={handleSubmit}>
-                        <label htmlFor="first">File</label>
-                        <input type="file" id="uploaded_file" name="uploaded_file" required />
-
-                        <label htmlFor="last">Text</label>
-                        <input type="text" id="n_speakers" name="n_speakers" required />
-
-                        <button type="submit">Submit</button>
-                    </form>
-        
-                    </div>
-                  </div>
+                <div style={{
+                    "paddingTop": '10px'
+                }} className="container-fluid">
+                    <button onClick={toggleModal} style={{'width': 'auto'}} type="button" className="btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        input
+                    </button>
                 </div>
-              </div>
+                
+                <div className={`modal fade ${show}`} style={{'display': `${display}`}} id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div className="modal-dialog modal-xl">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">Create Post</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" onClick={toggleModal} aria-label="Close"></button>
+                            </div>
+                            
+                            <div className="modal-body">
+                                <form className="row g-3" onSubmit={handleSubmit}>
+                                    <div className="col-12">
+                                        <label htmlFor="first" className="form-label">Thumbnail</label>
+                                        <input onChange={onSelectFile} type="file" id="uploaded_file" name="uploaded_file" className="form-control" required />
+                                        {selectedFile && <img className="img-fluid pt-3" src={preview}/>}
+                                    </div>
+                                    <div className="col-12">
+                                        <label htmlFor="last" className="form-label">Headline</label>
+                                        <input type="text" id="n_speakers" className="form-control" name="n_speakers" required />
+                                    </div>
+                                    <div className="col-12">
+                                        <button className="btn btn-success" type="submit">Submit</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </>
         )
     }
+
     return(
         <>
+            <Head>
+                <title>Post</title>
+                <meta name="description" content="Generated by create next app" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
             <Nav/>
             <div className="container-fluid">
-                <h3>Create Post</h3>
+                <h3>My Post</h3>
             </div>
         </>
     )
