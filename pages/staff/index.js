e61095606adb2c38bb5e565ef19cd83cd70ff97c
Ikbal Yaduar Taupik <Ikbal.ytaufik@gmail.com>
2022-10-25 12:14:59 +0700

    fix

--- pages/staff/index.js
+++ pages/staff/index.js
@@ -4,6 +4,7 @@ import Link from "next/link"
 import { useEffect, useState } from "react"
 import { Asterisk, Bootstrap, FiletypeJava, FiletypeJs, List, Microsoft } from "react-bootstrap-icons"
 import AppNavbar from "../../lib/components/AppNavbar"
+import SideNav from "../../lib/components/Staff/Navbar/SideNav"
 import StaffNav from "../../lib/components/StaffNav"
 import StaffNavTest from "../../lib/components/StaffNav/StaffNavTest"
 
@@ -52,45 +53,7 @@ export default function StaffDashboard(){
 
         <div className="container-fluid">
             <div className="row">
-                <div className="col-lg-2 col-md-2 col-sm-12" style={{'backgroundColor': '#f8f9f9', 'paddingTop': '0px'}}>
-                    <div className={"position-lg-sticky sticky-lg-top pt-0 fixed-height-overflow"} style={{'top': '76px'}}>
-                        <ul className="bd-links-nav list-unstyled mb-0 pb-3 pb-md-2 pe-lg-2 ps-2">
-                            <li className="bd-links-group py-2">
-                                <strong className="bd-links-heading d-flex w-100 align-items-center fw-semibold">
-                                    <h5>Data</h5>
-                                </strong>
-
-                                <ul className="list-unstyled fw-normal pb-2 small">
-                                    <li><Link href={'/staff/mahasiswa'}><a style={{'textDecoration': 'none'}} className="bd-links-link d-inline-block rounded active" aria-current="page"><h6>Mahasiswa</h6></a></Link></li>
-                                    <li><a style={{'textDecoration': 'none'}} className="bd-links-link d-inline-block rounded"><h6>Dosen</h6></a></li>
-                                    <li><a style={{'textDecoration': 'none'}} className="bd-links-link d-inline-block rounded"><h6>Kelas</h6></a></li>
-                                    <li><a style={{'textDecoration': 'none'}} className="bd-links-link d-inline-block rounded"><h6>Matakuliah</h6></a></li>
-                                    <li><a style={{'textDecoration': 'none'}} className="bd-links-link d-inline-block rounded"><h6>Jurusan</h6></a></li>
-                                </ul>
-                            </li>
-
-                            <li className="bd-links-group py-2">
-                                <strong className="bd-links-heading d-flex w-100 align-items-center fw-semibold">
-                                    <h5>Kelas</h5>
-                                </strong>
-
-                                <ul className="list-unstyled fw-normal pb-2 small">
-                                    <li><Link href={'/staff/mahasiswa'}><a style={{'textDecoration': 'none'}} className="bd-links-link d-inline-block rounded active" aria-current="page"><h6>Set Matakuliah</h6></a></Link></li>
-                                </ul>
-                            </li>
-
-                            <li className="bd-links-group py-2">
-                                <strong className="bd-links-heading d-flex w-100 align-items-center fw-semibold">
-                                    <h5>Blog</h5>
-                                </strong>
-
-                                <ul className="list-unstyled fw-normal pb-2 small">
-                                    <li><Link href={'/blog'}><a style={{'textDecoration': 'none'}} className="bd-links-link d-inline-block rounded active" aria-current="page"><h6>Post</h6></a></Link></li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
+                <SideNav/>
                 
                 <div className="col-lg-10 col-md-10 col-sm-12">
                     <div className="row px-3 ps-3 p-2">
